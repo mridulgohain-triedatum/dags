8956933e314304d5245ff02ed8da4abcca1b9a41
mridulgohain-triedatum <mridul.gohain@triedatum.com>
2026-07-13 07:02:09 +0000

    mssql test dag

--- conn_test_mssql.py
+++ conn_test_mssql.py
@@ -1,15 +1,14 @@
 from datetime import datetime
-
-from airflow import DAG
-from airflow.providers.microsoft.mssql.operators.mssql import MsSqlOperator
-from airflow.operators.python import PythonOperator
-from datetime import datetime
 import time
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 def wait_10_seconds(task_name):
     print(f"Starting {task_name}")
     time.sleep(10)
     print(f"Finished {task_name}")
+
 with DAG(
     dag_id="conn_test_mssql",
     start_date=datetime(2026, 1, 1),
@@ -17,19 +16,19 @@ with DAG(
     catchup=False,
     tags=["mssql"],
 ) as dag:
-    py_task=PythonOperator(
+    py_task = PythonOperator(
         task_id="task_1",
         python_callable=wait_10_seconds,
         op_args=["task_1"],
     )
 
-    view_databases = MsSqlOperator(
+    # 2. Swapped MsSqlOperator -> SQLExecuteQueryOperator
+    view_databases = SQLExecuteQueryOperator(
         task_id="view_databases",
-        mssql_conn_id="mssql_localhost",
+        conn_id="mssql_localhost",
         sql="""
             SELECT name FROM sys.databases;
         """,
         show_return_value_in_logs=True,
     )
-
     py_task >> view_databases
